16fa0f164cafa75f10b7152fda41d9048ece835f
Sidharth Menon <sidharth@startupmission.in>
2025-03-26 05:41:26 +0000

    chagnes

--- src/components/Countdown.svelte
+++ src/components/Countdown.svelte
@@ -41,19 +41,19 @@
 </script>
 
 <div class="flex items-center justify-center flex-wrap">
-	<div class="counter w-30 h-32 rounded-2xl text-white shadow font-bold font-heading text-6xl px-2 py-8 bg-[#1A496A] flex flex-col items-center justify-center">
+	<div class="counter w-24 h-28 rounded-2xl text-white shadow font-bold font-heading text-4xl px-2 py-8 bg-[#1A496A] flex flex-col items-center justify-center">
 		{dayText}
 		<div class="text-base font-light font-sans">Days</div>
 	</div>
-	<div class=" counter w-30 h-32 rounded-2xl text-white shadow font-bold font-heading text-6xl px-2 py-8 bg-[#1A496A] flex flex-col items-center justify-center">
+	<div class=" counter w-24 h-28 rounded-2xl text-white shadow font-bold font-heading text-4xl px-2 py-8 bg-[#1A496A] flex flex-col items-center justify-center">
 		{hourText}
 		<div class="text-base font-light font-sans">Hours</div>
 	</div>
-	<div class="counter w-30 h-32 rounded-2xl text-white shadow font-bold font-heading text-6xl px-2 py-8 bg-[#1A496A] flex flex-col items-center justify-center">
+	<div class="counter w-24 h-28 rounded-2xl text-white shadow font-bold font-heading text-4xl px-2 py-8 bg-[#1A496A] flex flex-col items-center justify-center">
 		{minText}
 		<div class="text-base font-light font-sans">Mins</div>
 	</div>
-	<div class="counter w-30 h-32 rounded-2xl text-white shadow font-bold font-heading text-6xl px-2 py-8 bg-[#1A496A] flex flex-col items-center justify-center">
+	<div class="counter w-24 h-28 rounded-2xl text-white shadow font-bold font-heading text-4xl px-2 py-8 bg-[#1A496A] flex flex-col items-center justify-center">
 		{secText}
 		<div class="text-base font-light font-sans">Secs</div>
 	</div>
