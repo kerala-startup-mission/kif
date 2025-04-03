522ce3ce631272ce45cdd93bd96b16cccbdc0b19
Sidharth Menon <sidharth@startupmission.in>
2025-04-03 04:27:15 +0000

    chang

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,13 @@ import tailwindcss from '@tailwindcss/vite';
 
 import svelte from '@astrojs/svelte';
 
+import lottie from "astro-integration-lottie";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
 
-  integrations: [svelte()]
+  integrations: [svelte(), lottie()]
 });
